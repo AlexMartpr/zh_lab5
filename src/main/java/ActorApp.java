818bab617dc08dd7cedbd7b14c6792089ccb45e4
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 23:27:00 +0300

    Scripted auto-commit on change (2021-12-10 23:27:00) by gitwatch.sh

--- src/main/java/ActorApp.java
+++ src/main/java/ActorApp.java
@@ -10,13 +10,14 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class ActorApp {
     private static final String HOST = "localhost";
     private static final int PORT = 8080;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("Akka_Lab_5");
         ActorRef cache = system.actorOf(Props.create(ActorCache.class));
@@ -25,7 +26,9 @@ public class ActorApp {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = FlowCreator.createFlow(materializer, cache);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost(HOST, PORT), materializer);
-        System.out.println("");
+        System.out.println("Serer online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+
 
     }
 

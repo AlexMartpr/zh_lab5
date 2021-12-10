d781ddf1057649bf35142e39c9219a8b790d655e
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 21:07:43 +0300

    Scripted auto-commit on change (2021-12-10 21:07:43) by gitwatch.sh

--- src/main/java/FlowCreator.java
+++ src/main/java/FlowCreator.java
@@ -2,19 +2,28 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 
 public class FlowCreator {
+    private static final String URL = "testUrl";
+    private static final String COUNT = "count";
+
     public FlowCreator() {};
     public Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer, ActorRef cache) {
         return Flow.of(HttpRequest.class).map(
           req -> {
-                
+                Query query = req.getUri().query();
+                String url;
+                if (query.get(URL).isPresent()) {
+                    url = query.get(URL).get();
+                } else {
+                    throw new Error();
+                }
               return null;
           }
         );
-        return null;
-    };
+    }
 }

ae0e78481b85bb39249c9e470cb6d74edb3c6c7a
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 21:20:09 +0300

    Scripted auto-commit on change (2021-12-10 21:20:09) by gitwatch.sh

--- src/main/java/FlowCreator.java
+++ src/main/java/FlowCreator.java
@@ -5,11 +5,13 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.Query;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import javafx.util.Pair;
 
 
 public class FlowCreator {
     private static final String URL = "testUrl";
     private static final String COUNT = "count";
+    private static final String 
 
     public FlowCreator() {};
     public Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer, ActorRef cache) {
@@ -22,9 +24,18 @@ public class FlowCreator {
                 } else {
                     throw new Exception("No URL parameter");
                 }
-                
-              return null;
+                int count;
+                try {
+                    if (query.get(COUNT).isPresent()) {
+                        count = Integer.parseInt(query.get(COUNT).get());
+                        return new Pair<>(url, count);
+                    } else {
+                        throw new Exception("No count parameter");
+                    }
+                } catch (NumberFormatException e) {
+                    throw new Exception("Can not parse value of count");
+                }
           }
-        );
+        ).mapAsync()
     }
 }

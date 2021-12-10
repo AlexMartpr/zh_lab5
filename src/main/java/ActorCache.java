767ae7d02fb1556cd12a8fdace15bd44a036483e
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 23:06:36 +0300

    Scripted auto-commit on change (2021-12-10 23:06:36) by gitwatch.sh

--- src/main/java/ActorCache.java
+++ src/main/java/ActorCache.java
@@ -1,2 +1,9 @@
-public class ActorCache {
+import akka.actor.AbstractActor;
+
+public class ActorCache extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }

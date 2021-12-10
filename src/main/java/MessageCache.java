a4d85d6228fc1a237833bd80b0e0c00b3ee630ac
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 21:27:08 +0300

    Scripted auto-commit on change (2021-12-10 21:27:08) by gitwatch.sh

--- src/main/java/MessageCache.java
+++ src/main/java/MessageCache.java
@@ -1,2 +1,17 @@
 public class MessageCache {
+    private final String url;
+    private final long time;
+
+    public MessageCache(String url, long time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public long getTime() {
+        return this.time;
+    }
 }

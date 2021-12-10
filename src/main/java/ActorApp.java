7637572cbf2fbd95a9b049371da28e66f00a9034
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 19:36:04 +0300

    Scripted auto-commit on change (2021-12-10 19:36:04) by gitwatch.sh

--- src/main/java/ActorApp.java
+++ src/main/java/ActorApp.java
@@ -1,6 +1,12 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class ActorApp {
 
@@ -8,6 +14,10 @@ public class ActorApp {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("Akka_Lab_5");
         ActorRef cache = system.actorOf(Props.create(Cache.class));
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = 
 
     }
 

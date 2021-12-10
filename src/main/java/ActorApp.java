b297f144c1441ee6aa48ef67772c54841333c701
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 19:27:40 +0300

    Scripted auto-commit on change (2021-12-10 19:27:40) by gitwatch.sh

--- src/main/java/ActorApp.java
+++ src/main/java/ActorApp.java
@@ -1,7 +1,9 @@
 public class ActorApp {
 
     public static void main(String[] args) {
-        
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+
     }
 
 }

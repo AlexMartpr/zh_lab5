ae38f0e7a52f23472c1a2c6436e3cb40146404ad
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 19:33:46 +0300

    Scripted auto-commit on change (2021-12-10 19:33:46) by gitwatch.sh

--- src/main/java/ActorApp.java
+++ src/main/java/ActorApp.java
@@ -6,8 +6,8 @@ public class ActorApp {
 
     public static void main(String[] args) {
         System.out.println("start!");
-        ActorSystem system = ActorSystem.create("routes");
-        ActorRef router = system.actorOf(Props.create(Router.class));
+        ActorSystem system = ActorSystem.create("Akka_Lab_5");
+        ActorRef cache = system.actorOf(Props.create(Cache.class));
 
     }
 

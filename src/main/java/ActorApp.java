75a23ae23f8144c48808142f71737133ae7e2e2f
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 23:22:24 +0300

    Scripted auto-commit on change (2021-12-10 23:22:24) by gitwatch.sh

--- src/main/java/ActorApp.java
+++ src/main/java/ActorApp.java
@@ -2,11 +2,15 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class ActorApp {
     private static final String HOST = "localhost";
@@ -20,6 +24,8 @@ public class ActorApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = FlowCreator.createFlow(materializer, cache);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost(HOST, PORT), materializer);
+        System.out.println("");
 
     }
 

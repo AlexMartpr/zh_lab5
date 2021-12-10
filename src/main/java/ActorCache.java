76cd5f627d9adc97991b719d1a837ded703000bf
AlexMartPr <alexmartpr@gmail.com>
2021-12-11 00:56:37 +0300

    Scripted auto-commit on change (2021-12-11 00:56:37) by gitwatch.sh

--- src/main/java/ActorCache.java
+++ src/main/java/ActorCache.java
@@ -5,13 +5,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ActorCache extends AbstractActor {
-    private final Map<String, Integer> store = new HashMap<>();
+    private final Map<String, Long> store = new HashMap<>();
     @Override
     public Receive createReceive() {
         return receiveBuilder().match(
                 Message.class,
                 message -> sender().tell(
-                        store.getOrDefault(message.getUrl(), -1),
+                        store.getOrDefault(message.getUrl(), (long) -1.0),
                         ActorRef.noSender()
                 )
         ).match(

8bb693c17159214cb33396720c52787e5928f31e
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 23:15:09 +0300

    Scripted auto-commit on change (2021-12-10 23:15:09) by gitwatch.sh

--- src/main/java/ActorCache.java
+++ src/main/java/ActorCache.java
@@ -9,7 +9,7 @@ public class ActorCache extends AbstractActor {
     public Receive createReceive() {
         return receiveBuilder().match(
                 Message.class,
-                message -> store.put()
+                message -> sender().tell()
         )
     }
 }

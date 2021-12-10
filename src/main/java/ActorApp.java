17a36cdc64af54fc62a06d10bd993d20ffbd1154
AlexMartPr <alexmartpr@gmail.com>
2021-12-10 19:31:33 +0300

    Scripted auto-commit on change (2021-12-10 19:31:33) by gitwatch.sh

--- src/main/java/ActorApp.java
+++ src/main/java/ActorApp.java
@@ -1,10 +1,13 @@
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class ActorApp {
 
     public static void main(String[] args) {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
+        ActorRef router = system.actorOf(Props.create(Router.class));
 
     }
 
